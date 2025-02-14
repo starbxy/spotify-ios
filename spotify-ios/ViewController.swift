7401caebe7a181d005dcd778e29f932b11878be8
Kaan <kaan.evcimen@sky.uk>
2025-02-14 13:39:30 +0000

    Initial Commit

---
+++ spotify-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotify-ios
+//
+//  Created by Kaan on 14/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ spotify-iosTests/spotify_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_iosTests.swift
+//  spotify-iosTests
+//
+//  Created by Kaan on 14/02/2025.
+//
+
+import Testing
+@testable import spotify_ios
+
+struct spotify_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
